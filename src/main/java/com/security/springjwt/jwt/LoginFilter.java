390aff5c2218e739a6d59c0a2cc77507b73e542f
Jeongho <fgh2585@gmail.com>
2024-04-04 21:45:24 +0900

    recommit

---
+++ src/main/java/com/security/springjwt/jwt/LoginFilter.java
@@ -0,0 +1,73 @@
+package com.security.springjwt.jwt;
+
+import com.security.springjwt.dto.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
+
+    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 username, password 추출
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println(username);
+
+        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+        //UserDetails
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        String username = customUserDetails.getUsername();
+
+        //특정한 role 값 추출, collection에서 authority 객체를 뽑아낸 뒤,
+        //iterator로 반복하여 내부 객체 뽑아냄
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        //jwt를 생성하는 메서드, jwt에 넣을 값들. 마지막은 jwt가 살아있을 시간
+        String token = jwtUtil.createJwt(username, role, 60 * 60 * 10L);
+
+        //jwt를 response의 헤더에 담아서 응답, header는 키 값과 접두사가 필요함
+        //이때 접두사 뒤에 띄어쓰기 필수!
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        System.out.println("failed");
+    }
+}
---
+++ src/main/java/com/security/springjwt/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.security.springjwt.repository;
+
+import com.security.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+
+    //username 받아 DB 테이블에서 회원을 조회하는 메소드
+    UserEntity findByUsername(String username);
+}
