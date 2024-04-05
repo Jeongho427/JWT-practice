377df7be219ad3dfe68b0eff93e6e02dc5390611
Jeongho <fgh2585@gmail.com>
2024-04-05 14:01:18 +0900

    CORS 설정

--- src/main/java/com/security/springjwt/config/SecurityConfig.java
+++ src/main/java/com/security/springjwt/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.security.springjwt.config;
 import com.security.springjwt.jwt.JWTFilter;
 import com.security.springjwt.jwt.JWTUtil;
 import com.security.springjwt.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -43,16 +48,46 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        http
+                .cors((cors) -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                //프론트 엔드의 포트 허용
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                //Get, Post 같은 모든 메서드 허용
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                //프론트 측에서 credential 설정하면 true로 해줘야함
+                                configuration.setAllowCredentials(true);
+                                //허용할 헤더
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                //허용을 가지고 있을 시간
+                                configuration.setMaxAge(3600L);
+
+                                //최종적으로 백엔드 측에서 클라이언트 측으로 헤더를 보내줄 때 Authorization에 jwt를 넣어서 보내주기 때문에
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        }));
+
+        //csrf disable
         http
                 .csrf((auth) -> auth.disable());
 
+        //Form 로그인 방식 disable
         http
                 .formLogin((auth) -> auth.disable());
 
+
+        //http basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
 
+        //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
